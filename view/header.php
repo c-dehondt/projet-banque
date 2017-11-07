eb0469e3f94f9ec96b760af1b85ade3f261cedef
c.dehondt <c.dehondt@live.com>
2017-11-07 14:23:07 +0100

    creation of ripo and addition of basic files for the project

---
+++ view/header.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0"/>
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow|Dancing+Script|Pacifico" rel="stylesheet">
+        <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.0/codemirror.min.css">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.6.0/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.6.0/css/froala_style.min.css" rel="stylesheet" type="text/css" /> -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="../css/main.css">
+        <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+      <script src="https://use.fontawesome.com/40917ba2bf.js"></script>
+    </head>
+    <body class="row">
+
+      <nav>
+          <div class="nav-wrapper  blue-grey darken-1">
+            <a href="home.php" class="brand-logo"><img class=" col m2 circle responsive-img" src="../img/logo.png" alt="logo du site"></a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+              <ul class="right hide-on-med-and-down">
+                <li><a href="../controller/addVehicule.php">Créer un produit</a></li>
+                <li><a href="home.php">Tous les produits</a></li>
+              </ul>
+
+              <ul class="side-nav" id="mobile-demo">
+                <li><a href="../controller/addVehicule.php">Créer un produit</a></li>
+                <li><a href="home.php">Tous les produits</a></li>
+              </ul>
+        </nav>
+
+        <main class="container row">
